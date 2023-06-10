e9b4da027a645c49e071cc2998293f52bbc8246e
hozlucas28 <hozlucas28@gmail.com>
2023-06-10 16:11:19 -0300

    Create constants

--- src/routes/app.router.tsx
+++ src/routes/app.router.tsx
@@ -1,16 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
+import ROUTES from './constants/ROUTES'
 
 const router = createBrowserRouter([
 	{
-		path: '/',
+		path: ROUTES.home.path,
 		element: <h1>Home</h1>,
 	},
 	{
-		path: '/signIn',
+		path: ROUTES.signIn.path,
 		element: <h1>Sign In</h1>,
 	},
 	{
-		path: '/signUp',
+		path: ROUTES.signUp.path,
 		element: <h1>Sign Up</h1>,
 	},
 ])
---
+++ src/routes/constants/ROUTES.ts
@@ -0,0 +1,18 @@
+const ROUTES = {
+	home: {
+		path: '/',
+		name: 'Home',
+	},
+
+	signIn: {
+		path: '/signIn',
+		name: 'Sign in',
+	},
+
+	signUp: {
+		path: '/signUp',
+		name: 'Sign up',
+	},
+}
+
+export default ROUTES
