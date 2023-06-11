371333279e37d9a5b82b9c92b2c778f7e71d4a23
hozlucas28 <hozlucas28@gmail.com>
2023-06-11 02:24:41 -0300

    Add paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@config': path.resolve(__dirname, './src/config'),
+			'@routes': path.resolve(__dirname, './src/routes'),
+			'@services': path.resolve(__dirname, './src/services'),
+		},
+	},
 })
