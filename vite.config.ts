d3e47f3447d2af4cbb680887836eaa4c173f3211
hozlucas28 <hozlucas28@gmail.com>
2023-06-11 03:29:14 -0300

    Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
+			'@': path.resolve(__dirname, './src'),
 			'@components': path.resolve(__dirname, './src/components'),
 			'@config': path.resolve(__dirname, './src/config'),
 			'@routes': path.resolve(__dirname, './src/routes'),
